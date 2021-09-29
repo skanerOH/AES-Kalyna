536808416c2a7c9f165401f103f041706362e442
Oleksandr Hutarevych <sashagutarevych@gmail.com>
2021-09-29 21:10:01 +0300

    finally done

---
+++ crypto_lab1/Kalyna.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace crypto_lab1
+{
+    class Kalyna
+    {
+        private KalynaBlock Key { get; }
+        private List<KalynaBlock> RoundsKeys { get; } = new List<KalynaBlock>();
+
+        public Kalyna(byte[] key)
+        {
+            Key = new KalynaBlock(key);
+            GenerateRoundsKeys();
+        }
+
+        private KalynaBlock GenerateKt()
+        {
+            var kt = new KalynaBlock
+            {
+                Data = new List<byte>
+                {
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5
+                }
+            };
+
+            kt.AddRoundKey(Key);
+
+            kt.SubBytes(StaticTables.Π);
+
+            kt.ShiftRows();
+
+            kt.MixColumns(StaticTables.Mds);
+
+            kt.Xor(Key);
+
+            kt.SubBytes(StaticTables.Π);
+
+            kt.ShiftRows();
+
+            kt.MixColumns(StaticTables.Mds);
+
+            kt.AddRoundKey(Key);
+
+            kt.SubBytes(StaticTables.Π);
+
+            kt.ShiftRows();
+
+            kt.MixColumns(StaticTables.Mds);
+
+            return kt;
+        }
+
+        public void GenerateRoundsKeys()
+        {
+            for (var i = 0; i <= 10; i++)
+                RoundsKeys.Add(new KalynaBlock());
+
+            var kt = GenerateKt();
+
+            for (var i = 0; i <= 10; i += 2)
+            {
+                var roundKey = RoundsKeys[i];
+                roundKey.Data = new List<byte>(StaticTables.V);
+                roundKey.ShiftLeft(i / 2);
+
+                var keyCopy = new KalynaBlock(Key);
+                keyCopy.RotateRight(32 * i);
+
+                roundKey.AddRoundKey(kt);
+                var copy = new KalynaBlock(roundKey);
+
+                roundKey.AddRoundKey(keyCopy);
+
+                roundKey.SubBytes(StaticTables.Π);
+
+                roundKey.ShiftRows();
+
+                roundKey.MixColumns(StaticTables.Mds);
+
+                roundKey.Xor(copy);
+
+                roundKey.SubBytes(StaticTables.Π);
+
+                roundKey.ShiftRows();
+
+                roundKey.MixColumns(StaticTables.Mds);
+
+                roundKey.AddRoundKey(copy);
+
+                RoundsKeys[i] = roundKey;
+            }
+
+            for (var i = 1; i <= 9; i += 2)
+            {
+                RoundsKeys[i].Data = RoundsKeys[i - 1].Data;
+                RoundsKeys[i].RotateLeft(56);
+            }
+        }
+
+        public KalynaBlock Encrypt(KalynaBlock plainText)
+        {
+            var cipherText = new KalynaBlock(plainText);
+            cipherText.AddRoundKey(RoundsKeys[0]);
+
+            for (var i = 1; i <= 9; i++)
+            {
+
+                cipherText.SubBytes(StaticTables.Π);
+
+                cipherText.ShiftRows();
+
+                cipherText.MixColumns(StaticTables.Mds);
+
+                cipherText.Xor(RoundsKeys[i]);
+            }
+
+            cipherText.SubBytes(StaticTables.Π);
+
+            cipherText.ShiftRows();
+
+            cipherText.MixColumns(StaticTables.Mds);
+
+            cipherText.AddRoundKey(RoundsKeys[10]);
+
+            return cipherText;
+        }
+
+        public KalynaBlock Decrypt(KalynaBlock cipherText)
+        {
+            var plainText = new KalynaBlock(cipherText);
+
+            plainText.SubRoundKey(RoundsKeys[10]);
+
+            plainText.MixColumns(StaticTables.MdsRev);
+
+            plainText.ShiftRowsRev();
+
+            plainText.SubBytes(StaticTables.ΠRev);
+
+            for (var i = 9; 1 <= i; --i)
+            {
+                plainText.Xor(RoundsKeys[i]);
+
+                plainText.MixColumns(StaticTables.MdsRev);
+
+                plainText.ShiftRowsRev();
+
+                plainText.SubBytes(StaticTables.ΠRev);
+            }
+
+            plainText.SubRoundKey(RoundsKeys[0]);
+            return plainText;
+        }
+    }
+}
---
+++ crypto_lab1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace crypto_lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AES
+            AESBlock input1 = new AESBlock(new byte[] { 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 });
+            Console.WriteLine("AES:");
+            Console.WriteLine("\tInput block: " + input1.ToString());
+
+            AES aes = new AES(new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 });
+            AESBlock chipheredBlock1 = aes.Encrypt(input1);
+            Console.WriteLine("\tEncripted block: " + chipheredBlock1.ToString());
+
+            AESBlock output1 = aes.Decrypt(chipheredBlock1);
+            Console.WriteLine("\tDecripted vlock: " + output1.ToString());
+
+
+            //Kalyna
+            KalynaBlock input2 = new KalynaBlock(new byte[] { 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 });
+            Console.WriteLine("Kalyna:");
+            Console.WriteLine("\tInput block: " + input2.ToString());
+
+            Kalyna kalyna = new Kalyna(new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 });
+            KalynaBlock chipheredBlock2 = kalyna.Encrypt(input2);
+            Console.WriteLine("\tEncripted block: " + chipheredBlock2.ToString());
+
+            KalynaBlock output2 = kalyna.Decrypt(chipheredBlock2);
+            Console.WriteLine("\tDecripted vlock: " + output2.ToString());
+
+            Console.Read();
+        }
+    }
+}
